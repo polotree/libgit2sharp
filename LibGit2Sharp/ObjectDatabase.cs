1027c769ac68759811a60e06e01fdf88f061ca80
Brendan Forster <brendan@github.com>
2015-02-04 12:33:29 +1100

    moved method into ObjectDatabase

--- LibGit2Sharp/ObjectDatabase.cs
+++ LibGit2Sharp/ObjectDatabase.cs
@@ -406,5 +406,31 @@ namespace LibGit2Sharp
 
             return gitObject.Sha.Substring(0, minLength.Value);
         }
+
+        /// <summary>
+        /// Returns whether merging <paramref name="ours"/> into <paramref name="theirs"/>
+        /// would result in merge conflicts.
+        /// </summary>
+        /// <param name="ours">The base tree to merge into.</param>
+        /// <param name="theirs">The tree to merge into <paramref name="ours"/>.</param>
+        /// <returns>True if the merge does not result in a conflict, false otherwise.</returns>
+        public virtual bool CanMergeWithoutConflict(Commit ours, Commit theirs)
+        {
+            using (var ourHandle = Proxy.git_object_peel(repo.Handle, ours.Id, GitObjectType.Tree, true))
+            using (var theirHandle = Proxy.git_object_peel(repo.Handle, theirs.Id, GitObjectType.Tree, true))
+            {
+                var ancestorCommit = repo.Commits.FindMergeBase(ours, theirs);
+
+                var ancestorHandle = ancestorCommit != null
+                    ? Proxy.git_object_peel(repo.Handle, ancestorCommit.Id, GitObjectType.Tree, false)
+                    : new NullGitObjectSafeHandle();
+
+                using (ancestorHandle)
+                using (var indexHandle = Proxy.git_merge_trees(repo.Handle, ancestorHandle, ourHandle, theirHandle))
+                {
+                    return !Proxy.git_index_has_conflicts(indexHandle);
+                }
+            }
+        }
     }
 }
